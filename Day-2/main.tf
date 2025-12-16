d1370af0014c4d411d250787f7d5787130aac024
Sai natha reddy Nooli <nooli.sainathareddy@gmail.com>
2025-12-16 14:26:56 -0600

    Added day 2 and 3

--- Day-2/main.tf
+++ Day-2/main.tf
@@ -1,16 +1,16 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 6.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
+  }
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_vpc" "my_first_vpc" {
-    cidr_block = "11.0.0.0/24"
+  cidr_block = "11.0.0.0/24"
 }
