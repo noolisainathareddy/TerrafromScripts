04d7ad91cc92ce5ffd3c177dedc8f2daba692a82
Sai natha reddy Nooli <nooli.sainathareddy@gmail.com>
2025-12-16 20:12:56 -0600

    added day 4

---
+++ Day-4/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.0"
+    }
+  }
+  backend "s3" {
+    bucket       = "sai-terraform-bucket-day4"
+    key          = "dev/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_default_vpc" "my-first-vpc" {
+  tags = {
+    Name = "Default VPC - sai"
+  }
+}
