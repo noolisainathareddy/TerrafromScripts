3e5902c7f550e43eba2565fef559b902b4503db1
Sai natha reddy Nooli <nooli.sainathareddy@gmail.com>
2025-12-21 10:12:01 -0600

    added Lift cycle concept on Day-9

---
+++ Day-9/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+    required_version = ">= 1.4"
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = ">= 6.4"    
+        }
+    }
+    backend "s3" {
+        bucket = "sai-nooli-terraform-day7"
+        key = "day9/dev/terraform.tfstate"
+        use_lockfile = true
+        encrypt = true
+        region = "us-east-1"
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+ }
