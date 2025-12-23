e15b61ff88daa10d2d074838708d3f11b9f9eaef
Sai natha reddy Nooli <nooli.sainathareddy@gmail.com>
2025-12-23 06:15:49 -0600

    Add day-11

--- Day-11/main.tf
+++ Day-11/main.tf
@@ -1,14 +1,8 @@
 resource "aws_instance" "ec2_instance" {
+  count = 2
   ami           = var.ami_id
   instance_type = var.instance_type
   tags = merge(var.proj-tags, var.aws-tags, {
     "Proj-Name" = lower(replace(var.proj-name, " ", "-"))
   })
-}
-
-resource "aws_s3_bucket" "s3-bucket" {
-  bucket = lower(replace(substr(var.s3-bucket-name, 0, 15), " ", "-"))
-  tags = {
-    "Proj-Name" = lower(replace(var.proj-name, " ", "-"))
-  }
 }
