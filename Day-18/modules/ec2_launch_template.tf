325f885a805250e4d1f01bdbd0d2f5829a27a0c8
Sai natha reddy Nooli <nooli.sainathareddy@gmail.com>
2026-03-12 23:38:08 -0500

    Added Auto Scaling Group

--- Day-18/modules/ec2_launch_template.tf
+++ Day-18/modules/ec2_launch_template.tf
@@ -3,5 +3,6 @@ resource "aws_launch_template" "nkit_ec2_launch_template" {
   image_id = "ami-02dfbd4ff395f2a1b"
   instance_type = "t3.micro"
   key_name = "east-1"
-  user_data = filebase64("../start.sh")
+  user_data = filebase64("${path.module}/start.sh")
+  vpc_security_group_ids = [aws_security_group.private_sg.id]
 }
