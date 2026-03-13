a16cf82f30895a9f3c629d627cdfa3eda163bfcc
Sai natha reddy Nooli <nooli.sainathareddy@gmail.com>
2026-03-13 00:06:06 -0500

    Added Bastion Host

--- Day-18/modules/auto_scaling_group.tf
+++ Day-18/modules/auto_scaling_group.tf
@@ -3,10 +3,8 @@ resource "aws_autoscaling_group" "nkit_auto_scaling_group" {
    max_size = 2
    min_size = 2
    desired_capacity = 2
-   availability_zones = [aws_subnet.nkit_private_subnet_1a.id, aws_subnet.nkit_private_subnet_1b.id]
+    vpc_zone_identifier = [aws_subnet.nkit_private_subnet_1a.id, aws_subnet.nkit_private_subnet_1b.id]
    launch_template {
      id = aws_launch_template.nkit_ec2_launch_template.id
-     name = aws_launch_template.nkit_ec2_launch_template.name
-     version = aws_launch_template.nkit_ec2_launch_template.latest_version
    }
 }
