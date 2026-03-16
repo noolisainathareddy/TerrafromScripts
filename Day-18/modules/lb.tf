ee0c69a7165b69c151c9472d8f465d9da956e78c
Sai natha reddy Nooli <nooli.sainathareddy@gmail.com>
2026-03-16 00:57:49 -0500

    Added ALB

--- Day-18/modules/lb.tf
+++ Day-18/modules/lb.tf
@@ -9,3 +9,21 @@ resource "aws_autoscaling_attachment" "nkit_dev_target_group" {
   lb_target_group_arn = aws_lb_target_group.nkit_dev_target_group.arn
   autoscaling_group_name = aws_autoscaling_group.nkit_auto_scaling_group.id
 }
+
+resource "aws_lb" "nkit_dev_lb" {
+  name = "Nkit-dev-lb"
+  internal = false
+  load_balancer_type = "application"
+  security_groups = [aws_security_group.public_sg.id]
+  subnets = [aws_subnet.nkit_public_subnet_1a.id, aws_subnet.nkit_public_subnet_1b.id]
+}
+
+resource "aws_lb_listener" "nkit_dev_lb_listner" {
+  load_balancer_arn = aws_lb.nkit_dev_lb.id
+  port = 80
+  protocol = "HTTP"
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.nkit_dev_target_group.arn
+  }
+}
