7494936de56c29eafe5ecc1ba622abad89e7aa21
Sai natha reddy Nooli <nooli.sainathareddy@gmail.com>
2026-03-25 11:40:44 -0500

    Updated node configs

--- Day-20/module/IAM/output.tf
+++ Day-20/module/IAM/output.tf
@@ -1,7 +1,7 @@
-output "eks_cluster_role_Arn" {
+output "eks_cluster_role_arn" {
   value = aws_iam_role.nkit_eks_cluster_role.arn
 }
 
-output "eks_node_role_Arn" {
+output "eks_node_role_arn" {
   value = aws_iam_role.nkit_eks_node_role.arn
 }
